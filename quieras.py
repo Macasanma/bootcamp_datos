eccbcffab5400d9a51bb1a4e4729ed211a338af6
Macasanma <m.sanchezmaeso@gmail.com>
2026-05-12 12:29:54 +0200

    Update quieras.py

--- quieras.py
+++ quieras.py
@@ -4,8 +4,10 @@ import numpy as np
 st.title('hello macasama!')
 st.subheader('hello arnau')
 
-lista = [4,5,6,6,97,23,54,43,67,78,33,98,90]
-media = np.mean(lista)
-st.selectbox(media, lista)
+st.pyplot
+
+
+
+
 
 
