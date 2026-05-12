f5c3b1c1756739ebacd917506b805371a5c69677
Macasanma <m.sanchezmaeso@gmail.com>
2026-05-12 12:23:58 +0200

    Update quieras.py

--- quieras.py
+++ quieras.py
@@ -1,4 +1,9 @@
 import streamlit as st
+import numpy as np
 
 st.title('hello macasama!')
 st.subheader('hello arnau')
+st.selectbox(media)
+
+lista = [4,5,6,6,97,23,54,43,67,78,33,98,90]
+media = np.mean(lista)
